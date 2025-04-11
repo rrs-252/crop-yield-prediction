5d26c37cac1c4a6870c7a656c9f1597504ec82b9
Rishabhraj Srivastava <rishabhraj.srivastava2021@vitstudent.ac.in>
2025-04-11 16:07:38 +0530

    Update deep_fusion.py

--- src/models/deep_fusion.py
+++ src/models/deep_fusion.py
@@ -1,49 +1,38 @@
 # deep_fusion.py
 import tensorflow as tf
-from tensorflow.keras.layers import LSTM, Dense, Embedding, Concatenate, Attention
 
-class DeepFusionModel(tf.keras.Model):
-    def __init__(self, num_districts=1000, num_crops=6):
+class TemporalFusionModel(tf.keras.Model):
+    def __init__(self, num_districts=1000):
         super().__init__()
         
-        # Temporal stream (Climate + NDVI/VCI)
-        self.temporal_encoder = tf.keras.Sequential([
-            LSTM(128, return_sequences=True),
-            Attention()
+        # Climate feature encoder
+        self.climate_encoder = tf.keras.Sequential([
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dropout(0.2)
         ])
-        
-        # Spatial features
-        self.district_embedding = Embedding(num_districts, 64)
-        
-        # Vegetation features
-        self.vegetation_encoder = Dense(32, activation='relu')
-        
-        # Crop embedding
-        self.crop_embedding = Embedding(num_crops, 32)
-        
-        # Fusion layers
-        self.fusion = Concatenate(axis=-1)
-        self.regressor = Dense(1, activation='linear')
-
-    def call(self, inputs):
-        # Process temporal features
-        temporal = self.temporal_encoder(inputs['temporal'])
-        
-        # Process vegetation
-        vegetation = self.vegetation_encoder(inputs['vegetation'])
         
         # District embedding
+        self.district_embedding = tf.keras.layers.Embedding(num_districts, 32)
+        
+        # Temporal attention
+        self.temporal_attention = tf.keras.layers.Attention()
+        
+        # Final regressor
+        self.regressor = tf.keras.Sequential([
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(1, activation='linear')
+        ])
+
+    def call(self, inputs):
+        # Input processing
+        climate_features = self.climate_encoder(inputs['climate'])
         district_emb = self.district_embedding(inputs['district'])
         
-        # Crop embedding
-        crop_emb = self.crop_embedding(inputs['crop'])
-        
-        # Feature fusion
-        fused = self.fusion([
-            tf.reduce_mean(temporal, axis=1),
-            vegetation,
-            district_emb,
-            crop_emb
+        # Temporal fusion
+        context = self.temporal_attention([
+            climate_features, 
+            tf.expand_dims(district_emb, 1)
         ])
         
-        return self.regressor(fused)
+        return self.regressor(context)
